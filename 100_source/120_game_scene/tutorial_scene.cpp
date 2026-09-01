65d3ca4184e3b665cb54b13747ebcd20e17d2938
A-nanana <tks2573420@stu.o-hara.ac.jp>
2026-09-01 15:11:36 +0900

    テキスト表示まで完成

--- 100_source/120_game_scene/tutorial_scene.cpp
+++ 100_source/120_game_scene/tutorial_scene.cpp
@@ -7,15 +7,33 @@
 //
 //Copyright (c) 2026 A.nanami All rights reserved.
 //------------------------------
+
+#include "../140_roading_from_other/file_roader.h"
+#include "../130_data_manager/133_music/music_manager.h"
 #include "tutorial_scene.h"
 
 void TutorialScene::UpdateTxt()
 {
+	//テキスト更新
+	if (!text_all_.GetCsv()->empty()) {
+		tutorial_txt_->SetText(text_all_.GetCsv()->front().text.c_str());
+		//再読み込み
+		tutorial_txt_->LoadResourceAll();
+		tutorial_txt_->SetUpAll();
+
+		//最初を削除
+		text_all_.GetCsv()->erase(text_all_.GetCsv()->begin());
+	}
 }
 
 void TutorialScene::Init() {
-
-
+	
+	MusicManager::GetInstance()->SetPlayMusic({ "tutorial",{0,0,0,0}, });
+	MusicManager::GetInstance()->SetDefficult();
+	//テキスト設定
+	std::string txt;//入力テキスト
+	FileRoader::GetInstance()->RoadTxt(txt,"testcsv.csv");
+	text_all_.ConvertCsv(txt.c_str());
 	//ゲームシーン側初期化
 	GameScene::Init();
 
@@ -23,12 +41,16 @@ void TutorialScene::Init() {
 
 void TutorialScene::SetUp()
 {
+	//テキスト準備
+	tutorial_txt_->LoadResourceAll();
+	UpdateTxt();
 	//ゲームシーン側セットアップ
 	GameScene::SetUp();
 }
 
 void TutorialScene::Finalize()
 {
+	tutorial_txt_->ReleaseResourceAll();
 
 	//ゲームシーン側ファイナライズ
 	GameScene::Finalize();
@@ -37,9 +59,14 @@ void TutorialScene::Finalize()
 
 Scene* TutorialScene::Update(float delta_time)
 {
-
 	//ゲームシーン側更新
 	GameScene::Update(delta_time);
+
+	//テキスト更新
+	if (text_all_.GetCsv()->front().time >= last_pop_up_) {
+		UpdateTxt();
+		last_pop_up_ = last_time_;
+	}
 
 	return next_scene_;
 }
@@ -50,5 +77,60 @@ void TutorialScene::Draw(int screen_handle)
 	GameScene::Draw(screen_handle);
 
 	//チュートリアル文の描画
+	tutorial_txt_->Draw(screen_handle,camera_);
 
+}
+
+//Csv関係
+
+void TutorialCsv::ConvertCsv(const char* csv)
+{
+	std::string csv_char(csv);
+	int i = 0; //カウント(文字)
+	int check = 0;//最終確認位置
+	CsvLavel lavel = k_csv_time; //Csv項目
+	CsvData pool_data = { "test",0}; //初期データ
+	std::string text_moto;//変換前テキスト
+
+	while (true)
+	{
+		//区切りなら分岐
+		if (csv_char[i] == ',' || csv_char[i] == '\n') {
+
+			text_moto = std::string(csv_char[check], csv_char[i - 1]);
+
+			//ラベルで変換
+			switch (lavel)
+			{
+			case k_csv_time:
+
+				pool_data.time = std::stoi(text_moto);
+				
+				check = i;
+				lavel = k_csv_text;
+				break;
+			case k_csv_text:
+
+				pool_data.text = text_moto;
+
+				check = i;
+				lavel = k_csv_time;
+				break;
+			default:
+			}
+			
+
+		}
+		//改行で代入
+		if (csv_char[i] == '\n') {
+			csv_data_.push_back(pool_data);
+		}
+		++i;
+
+		//最後ならそこで終了
+		if (csv_char.size() >= i) {
+			break;
+		}
+
+	}
 }
--- 100_source/120_game_scene/tutorial_scene.h
+++ 100_source/120_game_scene/tutorial_scene.h
@@ -9,13 +9,46 @@
 //------------------------------
 #pragma once
 
+#include <list>
+
 #include "game_scene.h"
 #include "../110_drawing_tools/text_node.h"
 
+
+//チュートリアルテキスト用CSVファイル
+//Csvデータチャンク
+struct CsvData
+{
+	std::string text;//テキスト
+	int time;//時間
+};
+//実際のデータ
+//データの並び
+enum CsvLavel {
+	k_csv_time, //時間
+	k_csv_text, //テキスト
+
+	k_csv_max
+};
+
+class TutorialCsv {
+protected:
+	std::list<CsvData> csv_data_; //Csvのリスト
+public:
+	void ConvertCsv(const char* csv);//コンバーター
+	std::list<CsvData>* GetCsv() { return &csv_data_; } //Csvの取得
+
+};
+
+//チュートリアルシーンクラス
 class TutorialScene:GameScene
 {
 protected:
 	TextNode* tutorial_txt_; //チュートリアル表示文
+	TutorialCsv text_all_;
+	const int text_pop_time_ = 3000;//テキスト表示時間
+
+	int last_pop_up_ = 0;//テキスト表示開始時間
 
 public:
 	//テキスト更新
